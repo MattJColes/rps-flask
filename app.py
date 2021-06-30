27190fdce59221e71d8f3d166b6f3a235dc01632
Matt Coles <mattjcoles@outlook.com>
2021-06-30 10:03:23 +1000

    changed port and apprunner.yaml

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 import random
 
 app = Flask(__name__)
-PORT = 8080
+PORT = 5000
 
 title = 'Rock, paper, scissors'
 computer_choice_options = ['🪨', '📜', '✂️']
