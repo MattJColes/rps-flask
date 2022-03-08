e5d0c4f666b0e9801e87b77a327b3bd6b5da112c
Matt Coles <mattjcoles@outlook.com>
2022-03-09 08:50:35 +1100

    modified title in app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# Rock Paper Scissors example app
 # Using gunicorn "gunicorn --bind 0.0.0.0:8080 wsgi:app"
 from flask import Flask, render_template, request, redirect, url_for
 import random
@@ -8,12 +9,14 @@ PORT = 8080
 title = 'Rock, paper, scissors'
 computer_choice_options = ['🪨', '📜', '✂️']
 
-@app.route('/')
-@app.route('/index', methods = ['GET'])
-def index():
-    return render_template('index.html', title = title)
 
-@app.route('/', methods = ['POST'])
+@app.route('/')
+@app.route('/index', methods=['GET'])
+def index():
+    return render_template('index.html', title=title)
+
+
+@app.route('/', methods=['POST'])
 def result():
     choice = request.form['choice']
     computer_choice = random.choice(computer_choice_options)
@@ -21,8 +24,10 @@ def result():
         outcome = "You tied with the computer"
     elif choice == "🪨" and computer_choice == "📜" or choice == "📜" and computer_choice == "✂️" or choice == "✂️" and computer_choice == "🪨":
         outcome = "You lost to the computer"
-    else: outcome = "You won over the computer"
-    return render_template('result.html', title = title, choice = choice, computer_choice = computer_choice, outcome = outcome)
+    else:
+        outcome = "You won over the computer"
+    return render_template('result.html', title=title, choice=choice, computer_choice=computer_choice, outcome=outcome)
+
 
 if __name__ == '__main__':
-   app.run(debug = False, host="0.0.0.0", port=PORT)
+    app.run(debug=False, host="0.0.0.0", port=PORT)
